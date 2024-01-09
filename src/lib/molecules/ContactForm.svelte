c6ccf5b0435d3b504267a27bb8c72157e9d46400
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-01-09 22:07:00 +0100

    e-mail koppelen met account
    Dit is gelukt d.m.v van web3forms dit is wel een gratis versie. Overleggen of zij eventueel pro willen voor betere futures.
    
    Nog als team overleggen of we nog meet input types willen!

--- src/lib/molecules/ContactForm.svelte
+++ src/lib/molecules/ContactForm.svelte
@@ -3,33 +3,48 @@
 </script>
 
 <section>
-<ContactInfo/>
+    <ContactInfo/>
 
-<form>
-    <label for="name">Voornaam:</label>
-    <input type="text" id="name" name="name" placeholder="Voer je voornaam in">
+    <form action="https://api.web3forms.com/submit" method="POST">
 
-    <label for="lastname">Achternaam:</label>
-    <input type="text" id="lastname" name="lastname" placeholder="Voer je achternaam in">
+        <input type="hidden" name="access_key" value="59b62920-afbb-4637-be5f-736618bec604">
 
-    <label for="message">Bericht:</label>
-    <textarea id="message" name="message" cols="30" rows="10" placeholder="Typ hier je bericht"></textarea>
+        <!-- Required: If submitting form without Javascript.
+       Default web3forms output is in JSON format. -->
+        <!-- Not Required: If javascript, use "window.location.hash" instead -->
+        <input type="hidden" name="redirect" value="https://web3forms.com/success">
 
-    <input type="submit" value="Verzenden">
-</form>
+        <!-- Optional: But Recommended: To Prevent SPAM Submission.
+             Make sure its hidden by default -->
+        <input type="checkbox" name="botcheck" class="hidden" style="display: none;">
+
+        <label for="name">Voornaam:</label>
+        <input type="text" id="name" name="name" placeholder="Voer je voornaam in" required>
+
+        <label for="lastname">Achternaam:</label>
+        <input type="text" id="lastname" name="lastname" placeholder="Voer je achternaam in" required>
+
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" name="email" placeholder="Voer je e-mailadres in" required>
+
+        <label for="message">Bericht:</label>
+        <textarea id="message" name="message" cols="30" rows="10" placeholder="Typ hier je bericht" required></textarea>
+
+        <input type="submit" value="Verzenden">
+    </form>
 </section>
 
 
 <style>
 
-    section{
+    section {
         display: flex;
         flex-direction: column;
         width: 50%;
         padding-right: var(--padding);
     }
-    
-    form{
+
+    form {
         display: flex;
         flex-direction: column;
         width: 100%;
@@ -37,19 +52,25 @@
         margin-top: 2rem;
     }
 
-    input[type="text"]{
+    input[type="text"] {
         border: none;
         padding: 0.5rem;
         border-radius: var(--bradius);
     }
 
-    textarea{
+    input[type="email"] {
         border: none;
         padding: 0.5rem;
         border-radius: var(--bradius);
     }
 
-    input[type="submit"]{
+    textarea {
+        border: none;
+        padding: 0.5rem;
+        border-radius: var(--bradius);
+    }
+
+    input[type="submit"] {
         margin-top: 1rem;
         border: none;
         border-radius: var(--bradius);
@@ -60,20 +81,20 @@
         font-weight: bold;
     }
 
-    input[type="submit"]:hover{
+    input[type="submit"]:hover {
         color: var(--button-color);
         background-color: var(--text-color);
     }
 
     @media screen and (max-width: 700px) {
 
-   section{
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-        padding-right: 0;
+        section {
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            padding-right: 0;
+        }
     }
-}
 
-    
+
 </style>
