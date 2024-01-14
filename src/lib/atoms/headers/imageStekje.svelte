bbb6e9cb2caaf08cfae63de6e848afe277078717
iBadr49 <badr.amara@hva.nl>
2024-01-14 21:06:13 +0100

    Accordion code aangepast
    Ik heb de code voor de accordion aangepast, waarbij ik gebruik heb gemaakt van de HTML elementen summary -details.

--- src/lib/atoms/headers/imageStekje.svelte
+++ src/lib/atoms/headers/imageStekje.svelte
@@ -3,32 +3,35 @@
 </script>
 
 <section class="image-section">
-	<img src={data.stekje.fotos[0].url} alt="foto van {data.naam}" />
-	<h1>{data.stekje.naam}</h1>
+    <img src={data.stekje.fotos[0].url} alt={`Foto van ${data.naam}`} loading="lazy" />
+    <h1>{data.stekje.naam}</h1>
 </section>
 
 <style>
-	section {
-		position: relative;
-		width: 100%;
-		background-color: var(--background-color);
-	}
+    section {
+        position: relative;
+        width: 100%;
+        background-color: var(--background-color);
+    }
 
-	.image-section {
-		height: 90vh;
-	}
+    .image-section {
+        height: 90vh;
+        overflow: hidden; 
+    }
 
-	img {
-		object-fit: cover;
-		width: 100%;
-		height: 100%;
-	}
+    img {
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+        display: block; /* weghalen van de extra spatie onder de image */
+    }
 
-	h1 {
-		position: absolute;
-		bottom: 2rem;
-		left: 2rem;
-		color: white;
-		font-size: 5vw;
-	}
+    h1 {
+        position: absolute;
+        bottom: 2rem;
+        left: 2rem;
+        color: white;
+        font-size: 2.5rem; 
+        margin: 0;
+    }
 </style>
